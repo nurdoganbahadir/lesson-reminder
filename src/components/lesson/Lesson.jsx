90afb766cba9b2d1b55f600f721aa184e484f123
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-25 00:49:18 +0300

    mixin ve variables oluşturuldu, gerekli yerlerde kullanıldı, proje tamamlandı

--- src/components/lesson/Lesson.jsx
+++ src/components/lesson/Lesson.jsx
@@ -10,8 +10,12 @@ const Lesson = ({ data }) => {
               <img src={image} alt={name} />
             </div>
             <div className="lesson-details">
-              <p className="lesson-name">Lesson Name: {name}</p>
-              <p className="lesson-hour">Lesson Hour: {hour}</p>
+              <p className="lesson name">
+                <span>Lesson Name:</span> {name}
+              </p>
+              <p className="lesson hour">
+                <span>Lesson Hour:</span> {hour}
+              </p>
             </div>
           </div>
         );
