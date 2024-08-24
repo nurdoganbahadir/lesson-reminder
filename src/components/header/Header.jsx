2f363f0f80bf0c41437d6b0ce16938f4e215efb0
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-24 21:50:11 +0300

    header görsel eklendi ve style özellikleri sağlandı

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,14 @@ import "./Header.scss";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <img
+        src="https://clarusway.com/wp-content/uploads/2022/12/clarusway-logo-black.png"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Header;
